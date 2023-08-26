95134a59bcf1749292970f6221f956fabec81638
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-08-26 14:49:28 +0900

    first commit

--- lib/cdk-workshop2-stack.ts
+++ lib/cdk-workshop2-stack.ts
@@ -1,19 +1,38 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as rds from 'aws-cdk-lib/aws-rds'
+import { Construct } from 'constructs'
+import { readFileSync } from 'fs'
 
 export class CdkWorkshop2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'CdkWorkshop2Queue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    const vpc = new ec2.Vpc(this, 'BlogVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    })
 
-    const topic = new sns.Topic(this, 'CdkWorkshop2Topic');
+    const webServer1 = new ec2.Instance(this, 'WordpressServer1', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const script = readFileSync('./lib/resources/user-data.sh', 'utf8')
+    webServer1.addUserData(script)
+
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80))
+
+    new CfnOutput(this, 'WordPressServer1PublicIPAdress', {
+      value: `http://${webServer1.instancePublicIp}`,
+    })
+
+    const dbServer = new rds.DatabaseInstance(this, 'WordPressDB', {
+      vpc,
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_31 }),
+    })
+
+    dbServer.connections.allowDefaultPortFrom(webServer1)
   }
 }
