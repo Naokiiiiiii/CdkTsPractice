a8b64e00e314fe31567700289cab61a98dc5d6c9
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-08-27 21:22:36 +0900

    feat: ec2のコンストラクタ作成

--- lib/cdk-workshop2-stack.ts
+++ lib/cdk-workshop2-stack.ts
@@ -1,10 +1,10 @@
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
+import { Stack, StackProps } from 'aws-cdk-lib'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import * as targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets'
 import * as rds from 'aws-cdk-lib/aws-rds'
 import { Construct } from 'constructs'
-import { readFileSync } from 'fs'
+import { WebServerInstance } from './constructs/web-server-instance'
 
 export class CdkWorkshop2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,20 +14,8 @@ export class CdkWorkshop2Stack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     })
 
-    const webServer1 = new ec2.Instance(this, 'WordpressServer1', {
+    const webServer1 = new WebServerInstance(this, 'WebServer1', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
-      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-    })
-
-    const script = readFileSync('./lib/resources/user-data.sh', 'utf8')
-    webServer1.addUserData(script)
-
-    // webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80))
-
-    new CfnOutput(this, 'WordPressServer1PublicIPAdress', {
-      value: `http://${webServer1.instancePublicIp}`,
     })
 
     const dbServer = new rds.DatabaseInstance(this, 'WordPressDB', {
@@ -37,7 +25,7 @@ export class CdkWorkshop2Stack extends Stack {
       databaseName: 'wordpress',
     })
 
-    dbServer.connections.allowDefaultPortFrom(webServer1)
+    dbServer.connections.allowDefaultPortFrom(webServer1.instance)
 
     const alb = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
       vpc,
@@ -50,12 +38,12 @@ export class CdkWorkshop2Stack extends Stack {
 
     listener.addTargets('ApplicationFleet', {
       port: 80,
-      targets: [new targets.InstanceTarget(webServer1, 80)],
+      targets: [new targets.InstanceTarget(webServer1.instance, 80)],
       healthCheck: {
         path: '/wp-includes/images/blank.gif',
       },
     })
 
-    webServer1.connections.allowFrom(alb, ec2.Port.tcp(80))
+    webServer1.instance.connections.allowFrom(alb, ec2.Port.tcp(80))
   }
 }
